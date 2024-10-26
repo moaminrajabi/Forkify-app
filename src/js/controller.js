e183243fdc0d096d108d454e17b2a05028ce68ba
Mohammad Amin Rajabi <mohammadaminrajabii2004@gmail.com>
2024-10-26 11:53:01 +0330

    test API

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,5 +1,3 @@
-const recipeContainer = document.querySelector(".recipe");
-
 const timeout = function (s) {
   return new Promise(function (_, reject) {
     setTimeout(function () {
@@ -11,4 +9,31 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      "https://forkify-api.herokuapp.com/api/v2/recipes?search=pizza"
+    );
+    const data = await res.json();
+    console.log(data);
 
+    if (!res.ok) throw new Error(`${data.message} - ${res.status}`);
+
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.imag_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredints: recipe.ingredints,
+    };
+
+    console.log(recipe);
+  } catch (error) {
+    alert(error);
+  }
+};
+showRecipe();

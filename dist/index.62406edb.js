edc3afc9f034c086dc65721cb135ae5b6ba7bd5c
Mohammad Amin Rajabi <mohammadaminrajabii2004@gmail.com>
2024-10-26 11:53:01 +0330

    test API

--- dist/index.62406edb.js
+++ dist/index.62406edb.js
@@ -1,11 +1,34 @@
-const recipeContainer = document.querySelector(".recipe");
 const timeout = function(s) {
     return new Promise(function(_, reject) {
         setTimeout(function() {
             reject(new Error(`Request took too long! Timeout after ${s} second`));
         }, s * 1000);
     });
-}; // https://forkify-api.herokuapp.com/v2
- ///////////////////////////////////////
+};
+// https://forkify-api.herokuapp.com/v2
+///////////////////////////////////////
+const showRecipe = async function() {
+    try {
+        const res = await fetch("https://forkify-api.herokuapp.com/api/v2/recipes?search=pizza");
+        const data = await res.json();
+        console.log(data);
+        if (!res.ok) throw new Error(`${data.message} - ${res.status}`);
+        let { recipe } = data.data;
+        recipe = {
+            id: recipe.id,
+            title: recipe.title,
+            publisher: recipe.publisher,
+            sourceUrl: recipe.source_url,
+            image: recipe.imag_url,
+            servings: recipe.servings,
+            cookingTime: recipe.cooking_time,
+            ingredints: recipe.ingredints
+        };
+        console.log(recipe);
+    } catch (error) {
+        alert(error);
+    }
+};
+showRecipe();
 
 //# sourceMappingURL=index.62406edb.js.map

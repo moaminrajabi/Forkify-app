ba4c20b6e58486903a1eb0b9c8886a2199675783
Mohammad Amin Rajabi <mohammadaminrajabii2004@gmail.com>
2024-10-26 18:51:18 +0330

    refactor MVC architecture

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,5 +1,7 @@
+import * as model from "./model";
 import icons from "../img/icons.svg";
 import icons from "url:../img/icons.svg";
+import "core-js/stable";
 import "regenerator-runtime/runtime";
 
 const recipeContainer = document.querySelector(".recipe");
@@ -30,33 +32,10 @@ const renderSpiner = function (parentEl) {
 const showRecipe = async function () {
   try {
     const id = window.location.hash.slice(1);
-    console.log(id);
-
     if (!id) return;
-
-    // loading
     renderSpiner(recipeContainer);
-
-    const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-    );
-    const data = await res.json();
-
-    if (!res.ok) throw new Error(`${data.message} - ${res.status}`);
-
-    let { recipe } = data.data;
-    recipe = {
-      id: recipe.id,
-      title: recipe.title,
-      publisher: recipe.publisher,
-      sourceUrl: recipe.source_url,
-      image: recipe.image_url,
-      servings: recipe.servings,
-      cookingTime: recipe.cooking_time,
-      ingredients: recipe.ingredients,
-    };
-
-    console.log(recipe);
+    await model.loadRecipe(id);
+    const { recipe } = model.state;
     // Rendering :
     const markup = `
  <figure class="recipe__fig">
